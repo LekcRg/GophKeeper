77a3063f42284d16b0ebf3291da178e634e95d5b
Lekc Rg <lekcrg@ya.ru>
2025-08-04 18:01:04 +0300

    feat(tests): tests for user service

--- internal/server/service/user_test.go
+++ internal/server/service/user_test.go
@@ -1,306 +1,402 @@
 package service
 
-// import (
-// 	"context"
-// 	"errors"
-// 	"testing"
+import (
+	"context"
+	"encoding/base64"
+	"errors"
+	"math/rand"
+	"testing"
 
-// 	"github.com/LekcRg/GophKeeper/internal/config"
-// 	"github.com/LekcRg/GophKeeper/internal/crypto"
-// 	"github.com/LekcRg/GophKeeper/internal/errs"
-// 	"github.com/LekcRg/GophKeeper/internal/mocks"
-// 	"github.com/LekcRg/GophKeeper/internal/models"
-// 	validation "github.com/go-ozzo/ozzo-validation/v4"
-// 	"github.com/jackc/pgerrcode"
-// 	"github.com/jackc/pgx/v5/pgconn"
-// 	"github.com/stretchr/testify/assert"
-// 	"github.com/stretchr/testify/mock"
-// 	"github.com/stretchr/testify/require"
-// )
+	"github.com/LekcRg/GophKeeper/internal/config"
+	"github.com/LekcRg/GophKeeper/internal/crypto"
+	"github.com/LekcRg/GophKeeper/internal/errs"
+	"github.com/LekcRg/GophKeeper/internal/mocks"
+	"github.com/LekcRg/GophKeeper/internal/models"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
 
-// type testUserServiceFunc func(ctx context.Context, req models.UserReq) (any, error)
+func TestRegister(t *testing.T) {
+	t.Parallel()
 
-// type testCase struct {
-// 	mockErr   error
-// 	checkErr  func(error) bool
-// 	req       models.UserReq
-// 	name      string
-// 	doNotMock bool
-// }
+	cfg, err := config.GetConfig([]string{})
+	require.NoError(t, err)
 
-// func TestRegister(t *testing.T) {
-// 	t.Parallel()
+	type test struct {
+		mockErr   error
+		req       models.UserReq
+		name      string
+		doNotMock bool
+		wantErr   bool
+	}
 
-// 	cfg, err := config.GetConfig([]string{})
-// 	require.NoError(t, err)
+	saltBytes, err := crypto.GenEncryptionSalt()
+	require.NoError(t, err)
 
-// 	tests := []testCase{
-// 		{
-// 			name: "success",
-// 			req:  models.UserReq{Login: "testuser", Password: "T3stP@as5word"},
-// 		},
-// 		{
-// 			name: "without password",
-// 			req:  models.UserReq{Login: "testuser"},
-// 			checkErr: func(err error) bool {
-// 				var v validation.Errors
-// 				return errors.As(err, &v)
-// 			},
-// 			doNotMock: true,
-// 		},
-// 		{
-// 			name:    "login already exist",
-// 			req:     models.UserReq{Login: "testuser", Password: "T3stP@as5word"},
-// 			mockErr: errs.ErrLoginAlreadyExists,
-// 			checkErr: func(err error) bool {
-// 				return errors.Is(err, errs.ErrLoginAlreadyExists)
-// 			},
-// 		},
-// 		{
-// 			name:    "unexpected repository error",
-// 			req:     models.UserReq{Login: "testuser", Password: "T3stP@as5word"},
-// 			mockErr: &pgconn.PgError{Code: pgerrcode.CannotConnectNow},
-// 			checkErr: func(err error) bool {
-// 				var pgErr *pgconn.PgError
-// 				return errors.As(err, &pgErr)
-// 			},
-// 		},
-// 	}
+	salt := base64.StdEncoding.EncodeToString(saltBytes)
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			t.Parallel()
+	tests := []test{
+		{
+			name: "success",
+			req: models.UserReq{
+				Login:        "testuser",
+				Password:     "T3stP@as5word",
+				EncryptedTag: "1234",
+				Salt:         salt,
+			},
+		},
+		{
+			name: "without password",
+			req: models.UserReq{
+				Login:        "testuser",
+				EncryptedTag: "1234",
+				Salt:         salt,
+			},
+			wantErr:   true,
+			doNotMock: true,
+		},
+		{
+			name: "login already exist",
+			req: models.UserReq{
+				Login:        "testuser",
+				Password:     "T3stP@as5word",
+				EncryptedTag: "1234",
+				Salt:         salt,
+			},
+			mockErr: errs.ErrLoginAlreadyExists,
+			wantErr: true,
+		},
+		{
+			name: "unexpected repository error",
+			req: models.UserReq{
+				Login:        "testuser",
+				Password:     "T3stP@as5word",
+				EncryptedTag: "1234",
+				Salt:         salt,
+			},
+			mockErr: &pgconn.PgError{Code: pgerrcode.CannotConnectNow},
+			wantErr: true,
+		},
+	}
 
-// 			runTestCase(t, cfg, &tt,
-// 				func(repo *mocks.MockUserRepo) {
-// 					repo.EXPECT().
-// 						CreateUser(mock.Anything, mock.Anything).
-// 						Return(42, tt.mockErr)
-// 				},
-// 				func(us *UserService) testUserServiceFunc {
-// 					return func(ctx context.Context, req models.UserReq) (any, error) {
-// 						return us.Register(ctx, req)
-// 					}
-// 				},
-// 			)
-// 		})
-// 	}
-// }
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 
-// func TestLogin(t *testing.T) {
-// 	t.Parallel()
+			repo := mocks.NewMockUserRepo(t)
+			us := NewUserService(repo, cfg)
 
-// 	cfg, err := config.GetConfig([]string{})
-// 	require.NoError(t, err)
+			userID := rand.Int()
+			if !tt.doNotMock {
+				repo.EXPECT().
+					CreateUser(mock.Anything, mock.Anything).
+					Return(userID, tt.mockErr)
+			}
 
-// 	const password = "T3stP@as5word"
-// 	hash, err := crypto.HashPassword(password)
-// 	require.NoError(t, err)
+			res, err := us.Register(context.Background(), tt.req)
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
 
-// 	tests := []testCase{
-// 		{
-// 			name: "success",
-// 			req:  models.UserReq{Login: "testuser", Password: password},
-// 		},
-// 		{
-// 			name: "without password",
-// 			req:  models.UserReq{Login: "testuser"},
-// 			checkErr: func(err error) bool {
-// 				var v validation.Errors
-// 				return errors.As(err, &v)
-// 			},
-// 			doNotMock: true,
-// 		},
-// 		{
-// 			name: "invalid password",
-// 			req:  models.UserReq{Login: "testuser", Password: "wrong"},
-// 			checkErr: func(err error) bool {
-// 				return errors.Is(err, errs.ErrInvalidCredentials)
-// 			},
-// 		},
-// 		{
-// 			name:    "not found",
-// 			req:     models.UserReq{Login: "testuser", Password: password},
-// 			mockErr: errs.ErrUserNotFound,
-// 			checkErr: func(err error) bool {
-// 				return errors.Is(err, errs.ErrInvalidCredentials)
-// 			},
-// 		},
-// 		{
-// 			name:    "unexpected error",
-// 			req:     models.UserReq{Login: "testuser", Password: password},
-// 			mockErr: &pgconn.PgError{Code: pgerrcode.CannotConnectNow},
-// 			checkErr: func(err error) bool {
-// 				var pgErr *pgconn.PgError
-// 				return errors.As(err, &pgErr)
-// 			},
-// 		},
-// 	}
+			require.NoError(t, err)
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			t.Parallel()
+			assert.NotEmpty(t, res.Key)
+		})
+	}
+}
 
-// 			runTestCase(t, cfg, &tt,
-// 				func(repo *mocks.MockUserRepo) {
-// 					repo.EXPECT().
-// 						GetUserByLogin(mock.Anything, tt.req.Login).
-// 						Return(models.User{
-// 							ID:           42,
-// 							Login:        tt.req.Login,
-// 							PasswordHash: hash,
-// 						}, tt.mockErr)
-// 				},
-// 				func(us *UserService) testUserServiceFunc {
-// 					return func(ctx context.Context, req models.UserReq) (any, error) {
-// 						return us.Login(ctx, req)
-// 					}
-// 				},
-// 			)
-// 		})
-// 	}
-// }
+func TestUpdateAPIKey(t *testing.T) {
+	t.Parallel()
 
-// func runTestCase(
-// 	t *testing.T,
-// 	cfg *config.Config,
-// 	tt *testCase,
-// 	mockFunc func(repo *mocks.MockUserRepo),
-// 	svcFunc func(us *UserService) testUserServiceFunc,
-// ) {
-// 	t.Helper()
+	cfg, err := config.GetConfig([]string{})
+	require.NoError(t, err)
 
-// 	repo := mocks.NewMockUserRepo(t)
-// 	us := NewUserService(repo, cfg)
+	password := "password"
+	passwordHash, err := crypto.HashPassword(password)
+	require.NoError(t, err)
 
-// 	if !tt.doNotMock {
-// 		mockFunc(repo)
-// 	}
+	type test struct {
+		mockErr         error
+		req             models.UserLogin
+		name            string
+		doNotMock       bool
+		doNotMockUpdate bool
+		GetUserErr      error
+		UpdateKeyErr    error
+		isErr           error
+		asErr           any
+	}
 
-// 	result, err := svcFunc(us)(context.Background(), tt.req)
-// 	if tt.checkErr == nil {
-// 		require.NoError(t, err)
-// 		assert.NotNil(t, result)
-// 	} else {
-// 		assert.Error(t, err)
-// 		assert.True(t, tt.checkErr(err))
-// 	}
-// }
+	errInternal := errors.New("internal")
+	tests := []test{
+		{
+			name: "success",
+			req: models.UserLogin{
+				Login:    "login",
+				Password: password,
+			},
+		},
+		{
+			name: "Empty password",
+			req: models.UserLogin{
+				Login: "login",
+			},
+			asErr:     validation.Errors{},
+			doNotMock: true,
+		},
+		{
+			name: "Invalid password",
+			req: models.UserLogin{
+				Login:    "login",
+				Password: "invalid",
+			},
+			isErr:           errs.ErrInvalidCredentials,
+			doNotMockUpdate: true,
+		},
+		{
+			name: "Not found user",
+			req: models.UserLogin{
+				Login:    "login",
+				Password: "password",
+			},
+			isErr:           errs.ErrInvalidCredentials,
+			GetUserErr:      errs.ErrUserNotFound,
+			doNotMockUpdate: true,
+		},
+		{
+			name: "Internal get user error",
+			req: models.UserLogin{
+				Login:    "login",
+				Password: "password",
+			},
+			isErr:           errInternal,
+			GetUserErr:      errInternal,
+			doNotMockUpdate: true,
+		},
+		{
+			name: "UpdateUserKey internal error",
+			req: models.UserLogin{
+				Login:    "login",
+				Password: "password",
+			},
+			isErr:        errInternal,
+			UpdateKeyErr: errInternal,
+		},
+	}
 
-// func TestChangePassword(t *testing.T) {
-// 	t.Parallel()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 
-// 	cfg, err := config.GetConfig([]string{})
-// 	require.NoError(t, err)
+			repo := mocks.NewMockUserRepo(t)
+			us := NewUserService(repo, cfg)
 
-// 	password := "T3stP@as5word"
-// 	newPassword := "N3wSup3rP@55wrd"
+			if !tt.doNotMock {
+				repo.EXPECT().
+					GetUserByLogin(mock.Anything, tt.req.Login).
+					Return(models.User{
+						Login:        tt.req.Login,
+						PasswordHash: passwordHash,
+					}, tt.GetUserErr)
 
-// 	hash, err := crypto.HashPassword(password)
-// 	require.NoError(t, err)
+				if !tt.doNotMockUpdate {
+					repo.EXPECT().
+						UpdateUserKey(mock.Anything, mock.Anything).
+						Return(tt.UpdateKeyErr)
+				}
+			}
 
-// 	type test struct {
-// 		mockGetErr      error
-// 		mockUpdateErr   error
-// 		checkErr        func(error) bool
-// 		req             models.UserChangePasswordReq
-// 		name            string
-// 		doNotMockGet    bool
-// 		doNotMockUpdate bool
-// 	}
+			res, err := us.UpdateAPIKey(context.Background(), tt.req)
+			if tt.isErr != nil || tt.asErr != nil {
+				assert.Error(t, err)
 
-// 	tests := []test{
-// 		{
-// 			name: "success",
-// 			req: models.UserChangePasswordReq{
-// 				Login:           "testuser",
-// 				CurrentPassword: password,
-// 				NewPassword:     newPassword,
-// 			},
-// 		},
-// 		{
-// 			name: "validation error",
-// 			req: models.UserChangePasswordReq{
-// 				Login:           "testuser",
-// 				CurrentPassword: password,
-// 				NewPassword:     "short",
-// 			},
-// 			checkErr: func(err error) bool {
-// 				var v validation.Errors
-// 				return errors.As(err, &v)
-// 			},
-// 			doNotMockGet: true,
-// 		},
-// 		{
-// 			name: "invalid current password",
-// 			req: models.UserChangePasswordReq{
-// 				Login:           "testuser",
-// 				CurrentPassword: "wrongPassword",
-// 				NewPassword:     newPassword,
-// 			},
-// 			checkErr: func(err error) bool {
-// 				return errors.Is(err, errs.ErrInvalidPassword)
-// 			},
-// 			doNotMockUpdate: true,
-// 		},
-// 		{
-// 			name: "user not found",
-// 			req: models.UserChangePasswordReq{
-// 				Login:           "testuser",
-// 				CurrentPassword: password,
-// 				NewPassword:     newPassword,
-// 			},
-// 			mockGetErr: errs.ErrUserNotFound,
-// 			checkErr: func(err error) bool {
-// 				return errors.Is(err, errs.ErrUserNotFound)
-// 			},
-// 			doNotMockUpdate: true,
-// 		},
-// 		{
-// 			name: "unexpected update error",
-// 			req: models.UserChangePasswordReq{
-// 				Login:           "testuser",
-// 				CurrentPassword: password,
-// 				NewPassword:     newPassword,
-// 			},
-// 			mockUpdateErr: &pgconn.PgError{Code: pgerrcode.CannotConnectNow},
-// 			checkErr: func(err error) bool {
-// 				var pgErr *pgconn.PgError
-// 				return errors.As(err, &pgErr)
-// 			},
-// 		},
-// 	}
+				if tt.isErr != nil {
+					assert.ErrorIs(t, err, tt.isErr)
+				}
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			t.Parallel()
+				if tt.asErr != nil {
+					assert.ErrorAs(t, err, &tt.asErr)
+				}
 
-// 			repo := mocks.NewMockUserRepo(t)
-// 			us := NewUserService(repo, cfg)
+				return
+			}
 
-// 			if !tt.doNotMockGet {
-// 				repo.EXPECT().
-// 					GetUserByLogin(context.Background(), tt.req.Login).
-// 					Return(models.User{
-// 						ID:           1,
-// 						Login:        tt.req.Login,
-// 						PasswordHash: hash,
-// 					}, tt.mockGetErr)
-// 			}
+			assert.NotNil(t, res.Key)
+		})
+	}
+}
 
-// 			if !tt.doNotMockGet && !tt.doNotMockUpdate {
-// 				repo.EXPECT().
-// 					UpdateUserPassword(context.Background(), mock.Anything).
-// 					Return(tt.mockUpdateErr)
-// 			}
+func TestChangePassword(t *testing.T) {
+	t.Parallel()
 
-// 			err := us.ChangePassword(context.Background(), tt.req)
-// 			if tt.checkErr == nil {
-// 				require.NoError(t, err)
-// 			} else {
-// 				assert.Error(t, err)
-// 				assert.True(t, tt.checkErr(err))
-// 			}
-// 		})
-// 	}
-// }
+	cfg, err := config.GetConfig([]string{})
+	require.NoError(t, err)
+
+	password := "T3stP@as5word"
+	newPassword := "N3wSup3rP@55wrd"
+
+	hash, err := crypto.HashPassword(password)
+	require.NoError(t, err)
+
+	type test struct {
+		mockGetErr      error
+		mockUpdateErr   error
+		checkErr        func(error) bool
+		req             models.UserChangePasswordReq
+		name            string
+		doNotMockGet    bool
+		doNotMockUpdate bool
+	}
+
+	tests := []test{
+		{
+			name: "success",
+			req: models.UserChangePasswordReq{
+				Login:           "testuser",
+				CurrentPassword: password,
+				NewPassword:     newPassword,
+			},
+		},
+		{
+			name: "validation error",
+			req: models.UserChangePasswordReq{
+				Login:           "testuser",
+				CurrentPassword: password,
+				NewPassword:     "short",
+			},
+			checkErr: func(err error) bool {
+				var v validation.Errors
+				return errors.As(err, &v)
+			},
+			doNotMockGet: true,
+		},
+		{
+			name: "invalid current password",
+			req: models.UserChangePasswordReq{
+				Login:           "testuser",
+				CurrentPassword: "wrongPassword",
+				NewPassword:     newPassword,
+			},
+			checkErr: func(err error) bool {
+				return errors.Is(err, errs.ErrInvalidPassword)
+			},
+			doNotMockUpdate: true,
+		},
+		{
+			name: "user not found",
+			req: models.UserChangePasswordReq{
+				Login:           "testuser",
+				CurrentPassword: password,
+				NewPassword:     newPassword,
+			},
+			mockGetErr: errs.ErrUserNotFound,
+			checkErr: func(err error) bool {
+				return errors.Is(err, errs.ErrUserNotFound)
+			},
+			doNotMockUpdate: true,
+		},
+		{
+			name: "unexpected update error",
+			req: models.UserChangePasswordReq{
+				Login:           "testuser",
+				CurrentPassword: password,
+				NewPassword:     newPassword,
+			},
+			mockUpdateErr: &pgconn.PgError{Code: pgerrcode.CannotConnectNow},
+			checkErr: func(err error) bool {
+				var pgErr *pgconn.PgError
+				return errors.As(err, &pgErr)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			repo := mocks.NewMockUserRepo(t)
+			us := NewUserService(repo, cfg)
+
+			if !tt.doNotMockGet {
+				repo.EXPECT().
+					GetUserByLogin(context.Background(), tt.req.Login).
+					Return(models.User{
+						ID:           1,
+						Login:        tt.req.Login,
+						PasswordHash: hash,
+					}, tt.mockGetErr)
+			}
+
+			if !tt.doNotMockGet && !tt.doNotMockUpdate {
+				repo.EXPECT().
+					UpdateUserPassword(context.Background(), mock.Anything).
+					Return(tt.mockUpdateErr)
+			}
+
+			err := us.ChangePassword(context.Background(), tt.req)
+			if tt.checkErr == nil {
+				require.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+				assert.True(t, tt.checkErr(err))
+			}
+		})
+	}
+}
+
+func TestCryptoParams(t *testing.T) {
+	type test struct {
+		name    string
+		id      int
+		wantErr bool
+		svcErr  error
+	}
+
+	tests := []test{
+		{
+			name: "Success",
+			id:   1,
+		},
+		{
+			name:    "Success",
+			id:      1,
+			wantErr: true,
+			svcErr:  errs.ErrUserNotFound,
+		},
+	}
+
+	cfg, err := config.GetConfig([]string{})
+	require.NoError(t, err)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			repo := mocks.NewMockUserRepo(t)
+			us := NewUserService(repo, cfg)
+
+			repo.EXPECT().
+				GetUserByID(mock.Anything, tt.id).
+				Return(models.User{
+					Salt:         "salt",
+					EncryptedTag: "tag",
+				}, tt.svcErr)
+
+			params, err := us.GetCryptoParams(context.Background(), tt.id)
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+
+			assert.NotEmpty(t, params.EncryptedTag)
+			assert.NotEmpty(t, params.Salt)
+		})
+	}
+}
